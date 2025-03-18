968f3a1c9a38a448b2f70e294463e8c8af7ab31a
Leone Sacha <LeoneS1@s2.rpn.ch>
2025-03-18 08:35:54 +0100

    Updated JT, 4 days missing

--- DIMEN/obj/Debug/net8.0/DIMEN.AssemblyInfo.cs
+++ DIMEN/obj/Debug/net8.0/DIMEN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee9f0baa285b449ccf3045b16981b745fd95265d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9a0e93d5fb696a1011637a44783b011eca3a2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

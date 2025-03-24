bf07f9e1169b1137298db07caa41cf697b55c206
Sacha Leone <Sacha.Leone@rpn.ch>
2025-03-24 16:12:49 +0100

    Updated report

--- DIMEN/obj/Release/net8.0/DIMEN.AssemblyInfo.cs
+++ DIMEN/obj/Release/net8.0/DIMEN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07c7ab92a13292b5224ac88b2da4458b9664f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4a954d4ce274dcbb0a3134c3ffb50760514a8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

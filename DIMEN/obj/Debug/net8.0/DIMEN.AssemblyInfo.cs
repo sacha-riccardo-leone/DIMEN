cd6b7bcc5c85decb61884655bd4933a720435da8
Sacha Leone <Sacha.Leone@rpn.ch>
2025-03-31 12:13:48 +0200

    Added powerpoint

--- DIMEN/obj/Debug/net8.0/DIMEN.AssemblyInfo.cs
+++ DIMEN/obj/Debug/net8.0/DIMEN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8907544a8d6203de7a3160d64cf43e7eac92a89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9591e34ab23f5c7b471a6d60c4a068bb9a8942")]
 [assembly: System.Reflection.AssemblyProductAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DIMEN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
